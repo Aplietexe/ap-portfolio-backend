f1d1f4b8c3f92da512b2b91179f438ab640addc8
Pietro Palombini <pietropalo@icloud.com>
2023-03-19 15:06:58 -0300

    feat(Experience): create repository

---
+++ src/main/java/com/apportfoliobackend/apportfoliobackend/repository/ExperienceRepository.java
@@ -0,0 +1,9 @@
+package com.apportfoliobackend.apportfoliobackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apportfoliobackend.apportfoliobackend.model.Experience;
+
+public interface ExperienceRepository extends JpaRepository<Experience, Long> {
+
+}
