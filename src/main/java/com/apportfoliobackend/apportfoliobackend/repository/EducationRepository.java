dea8ce818be119a58e5ad62e9489b4e35f0844a3
Pietro Palombini <pietropalo@icloud.com>
2023-03-19 15:20:12 -0300

    feat(Education): create repository

---
+++ src/main/java/com/apportfoliobackend/apportfoliobackend/repository/EducationRepository.java
@@ -0,0 +1,9 @@
+package com.apportfoliobackend.apportfoliobackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apportfoliobackend.apportfoliobackend.model.Education;
+
+public interface EducationRepository extends JpaRepository<Education, Long> {
+
+}
