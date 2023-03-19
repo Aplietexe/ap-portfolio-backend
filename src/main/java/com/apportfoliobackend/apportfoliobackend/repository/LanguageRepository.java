47fce3db9b435a186f5fa77474a60e490455ab95
Pietro Palombini <pietropalo@icloud.com>
2023-03-19 15:38:12 -0300

    feat(Language): create repository

---
+++ src/main/java/com/apportfoliobackend/apportfoliobackend/repository/LanguageRepository.java
@@ -0,0 +1,9 @@
+package com.apportfoliobackend.apportfoliobackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.apportfoliobackend.apportfoliobackend.model.Language;
+
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+
+}
