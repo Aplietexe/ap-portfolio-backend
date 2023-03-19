f1adf6fab0f52169b0642bd8b6eabcab961d3a58
Pietro Palombini <pietropalo@icloud.com>
2023-03-19 15:24:47 -0300

    feat(Skill): create model

---
+++ src/main/java/com/apportfoliobackend/apportfoliobackend/model/Skill.java
@@ -0,0 +1,25 @@
+package com.apportfoliobackend.apportfoliobackend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String image;
+    private String title;
+    private String subtitle;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
